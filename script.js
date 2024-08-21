601860c9bf65995a24d9bd1c5a596258801eea89
Omar Davidson <omardavidson227@gmail.com>
2024-08-21 08:26:37 -0400

    made two for loops; the first starts counting from 1 and while i is less than or equal to 5 it will continue incrementing; the second starts counting from 0 and while i is less than or equal to 4 it will continue incrementing.

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let firstname = "Omar";
 let lastname = "Davidson";
 
 let fullname = `${firstname} ${lastname}`
-
 console.log(fullname)
 
 let detroitPopulation = 620376
@@ -14,3 +13,15 @@ console.log(calculation)
 let javaScriptIsCool = true
 
 let colors = ["blue", "red", "green", "yellow", "orange", "purple", "pink"]
+
+// Write a for loop that counts from 1 to 5 and logs each number to the console.
+
+// initializing the count at 1; if i less or equal to 5 continue looping and incrementing i
+for (let i = 1; i <= 5; i++) {
+    console.log(i) 
+}
+
+// starting count at 0 (let i = 0;); if i is less than or equal to 4 (i <= 4;) then continue incrementing i (i++)
+for (let i = 0; i <= 4; i++) {
+    console.log(i)
+}
